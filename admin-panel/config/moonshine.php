a386186459d62a56a7cae3e199bc97cfe19dfae7
Víctor J <maximovj@outlook.com>
2024-11-03 17:19:03 -0600

    * Se modifica los iconos de la plataforma

--- admin-panel/config/moonshine.php
+++ admin-panel/config/moonshine.php
@@ -18,7 +18,7 @@ return [
 
     'route' => [
         'domain' => env('MOONSHINE_URL', ''),
-        'prefix' => env('MOONSHINE_ROUTE_PREFIX', 'admin'),
+        'prefix' => env('MOONSHINE_ROUTE_PREFIX', '/'),
         'single_page_prefix' => 'page',
         'index' => 'moonshine.index',
         'middlewares' => [
