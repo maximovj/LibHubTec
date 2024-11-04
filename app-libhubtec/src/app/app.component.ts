8039a65d3e6c666d427df5da250c5d7da0644641
Víctor J <maximovj@outlook.com>
2024-11-03 18:36:55 -0600

    * Se instala PrimeNG
    // !#Cambios
    
    - Se instala PrimeNG
    @see https://primeng.org/installation
    @see https://primeng.org/configuration
    @see https://primeng.org/icons
    
    Se usa los sig. comandos:
    $ npm install primeng
    $ npm install primeicons
    
    - Modificar el archivo `styles.css`
    @import "primeng/resources/themes/lara-light-blue/theme.css";
    @import "primeng/resources/primeng.css";
    @import "primeicons/primeicons.css";
    
    - Configuar el app.component.ts
    ``
    import { Component, OnInit } from '@angular/core';
    import { PrimeNGConfig } from 'primeng/api';
    
    @Component({
        selector: 'app-root',
        templateUrl: './app.component.html'
    })
    export class AppComponent implements OnInit {
    
        constructor(private primengConfig: PrimeNGConfig) {}
    
        ngOnInit() {
          this.primengConfig.ripple = true;
        }
    
    }
    ``

--- app-libhubtec/src/app/app.component.ts
+++ app-libhubtec/src/app/app.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+import { ButtonModule } from 'primeng/button';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    ButtonModule,
+    CommonModule,
+    RouterOutlet,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent  implements OnInit {
+
   title = 'app-libhubtec';
+  private primengConfig: PrimeNGConfig = inject(PrimeNGConfig);
+
+  ngOnInit() {
+      this.primengConfig.ripple = true;
+  }
+
 }
