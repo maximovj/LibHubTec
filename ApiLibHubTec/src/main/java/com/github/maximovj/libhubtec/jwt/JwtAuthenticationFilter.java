b11dbccc79b8bfdf7c41cae7f3c3f26216c97628
Víctor J <maximovj@outlook.com>
2024-11-10 22:05:01 -0600

    * Se agrega rutas protegidas por JWT

--- ApiLibHubTec/src/main/java/com/github/maximovj/libhubtec/jwt/JwtAuthenticationFilter.java
+++ ApiLibHubTec/src/main/java/com/github/maximovj/libhubtec/jwt/JwtAuthenticationFilter.java
@@ -3,27 +3,57 @@ package com.github.maximovj.libhubtec.jwt;
 import java.io.IOException;
 
 import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.github.maximovj.libhubtec.services.JwtService;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 
 @Component
+@RequiredArgsConstructor
 public class JwtAuthenticationFilter  extends OncePerRequestFilter {
+	
+	private final JwtService jwtService;
+	private final UserDetailsService userDetailsService;
 
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		final String token = getTokenFromRequest(request);
+		final String username;
 		
 		if(token == null) {
 			filterChain.doFilter(request, response);
 			return; 
+		}
+		
+		username=jwtService.getUsernameFromToken(token);
+		if(username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+			UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
+			
+			if( jwtService.isTokenValid(token, userDetails)) {
+				UsernamePasswordAuthenticationToken authToken;
+				authToken = new UsernamePasswordAuthenticationToken(
+						userDetails, 
+						null, 
+						userDetails.getAuthorities());
+				authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+				
+				SecurityContextHolder.getContext().setAuthentication(authToken);
+			}
+			
 		}
 		
 		filterChain.doFilter(request, response);
--- ApiLibHubTec/src/main/java/com/github/maximovj/libhubtec/services/JwtService.java
+++ ApiLibHubTec/src/main/java/com/github/maximovj/libhubtec/services/JwtService.java
@@ -4,12 +4,17 @@ import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import com.github.maximovj.libhubtec.user.User;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
@@ -27,18 +32,58 @@ public class JwtService {
 	private String getToken(Map<String, Object> extraClaims, UserDetails user) {
 		return Jwts
 				.builder()
-				.setClaims(extraClaims)
-				.setSubject(user.getUsername())
-				.setIssuedAt(new Date(System.currentTimeMillis()))
-				.setExpiration(new Date(System.currentTimeMillis() *100 * 60 * 24))
-				.signWith(getKey(), SignatureAlgorithm.HS256)
+				.claims(extraClaims)
+				.subject(user.getUsername())
+				.issuedAt(new Date(System.currentTimeMillis()))
+				.expiration(new Date(System.currentTimeMillis() *100 * 60 * 24))
+				.signWith(getKey())
 				.compact();
 	}
 	
 	private Key getKey () {
 		byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
 		return Keys.hmacShaKeyFor(keyBytes);
-		
 	}
+	
+	public SecretKey getSecretKey() {
+        // Define una clave secreta en formato de cadena (asegúrate de usar una clave segura)
+        String secret = SECRET_KEY;
+        
+        // Convierte la clave secreta en bytes
+        byte[] keyBytes = secret.getBytes();
+        
+        // Crea la `SecretKey` usando `SecretKeySpec` y el algoritmo HMAC SHA-256
+        return new SecretKeySpec(keyBytes, 0, keyBytes.length, "HmacSHA256");
+    }
+
+	public String getUsernameFromToken(String token) {
+		return this.getClaim(token, Claims::getSubject);
+	}
+
+	public boolean isTokenValid(String token, UserDetails userDetails) {
+		
+		final String username = this.getUsernameFromToken(token);
+		return (username.equals(userDetails.getUsername()) && !this.isTokenExpirated(token) );
+	}
+	
+	private Claims getAllClaims(String token) {
+		return Jwts.parser().setSigningKey(getKey()).build().parseSignedClaims(token).getPayload();
+	}
+	
+	public <T> T getClaim(String token, Function<Claims, T> claimsResolver) 
+	{
+		final Claims claimns = this.getAllClaims(token);
+		return  claimsResolver.apply(claimns);
+	}
+	
+	private Date getExpiration(String token) 
+	{
+		return this.getClaim(token, Claims::getExpiration);
+	}
+	
+	private boolean isTokenExpirated(String token) {
+		return getExpiration(token).before(new Date());
+	}
+	
 	
 }
