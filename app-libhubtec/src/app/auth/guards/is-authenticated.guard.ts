a88d6d8c67b17f8999ead8268b14249206bff6e7
Víctor J <maximovj@outlook.com>
2024-11-14 15:48:33 -0600

    * Se modifica guard

--- app-libhubtec/src/app/auth/guards/is-authenticated.guard.ts
+++ app-libhubtec/src/app/auth/guards/is-authenticated.guard.ts
@@ -8,6 +8,10 @@ export const isAuthenticatedGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
 
+  if(authService.authStatus() ===  AuthStatus.checking) {
+    return false;
+  }
+
   if(authService.authStatus() === AuthStatus.notAuthenticated) {
     router.navigateByUrl('/auth/login');
     return false;
