901c4be871e8a52ebe21b1ed8425bacbc6ce4970
Víctor J <maximovj@outlook.com>
2024-11-14 19:00:01 -0600

    * Se agrega interfaces en Angular 18.x

--- app-libhubtec/src/app/auth/interfaces/index.ts
+++ app-libhubtec/src/app/auth/interfaces/index.ts
@@ -1,2 +1,4 @@
 export * from './user.interface';
 export * from './verify-token-response.interface';
+export * from './account-details-response.interface';
+export * from './payload.interface';
--- app-libhubtec/src/app/auth/interfaces/user.interface.ts
+++ app-libhubtec/src/app/auth/interfaces/user.interface.ts
@@ -1,5 +1,13 @@
 export interface User {
-  username    :string;
-  email       : string;
-  password    : string;
+  id:        number;
+  name:      string;
+  last_name: string;
+  sex:       string;
+  age:       string;
+  grade:     string;
+  shift:     string;
+  photo:     null;
+  bio:       string;
+  username:  string;
+  email:     string;
 }
