51175511ed4f50310343e4c1d670cc5d2b67f9be
Víctor J <maximovj@outlook.com>
2024-11-24 16:59:01 -0600

    * dev-springboot-v3 : Se agrega `jackson-databind` como depedencia
    // !# Cambios
    - Se agrega `jackson-databind` como depedencia
    - Se crea un convertir (Converter) `JsonToListConverter` para convertir json a list de Java
    
    - `jackson-databind` : sirve para mapear un json, y convertirlo a string, es una libreria para json.

---
+++ ApiLibHubTec/src/main/java/com/github/maximovj/libhubtec/converter/JsonToListConverter.java
@@ -0,0 +1,39 @@
+package com.github.maximovj.libhubtec.converter;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+import java.util.ArrayList;
+import java.util.List;
+
+@Converter
+public class JsonToListConverter implements AttributeConverter<List<String>, String> {
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public String convertToDatabaseColumn(List<String> attribute) {
+        if (attribute == null || attribute.isEmpty()) {
+            return "[]"; // Devuelve un arreglo vacío en JSON
+        }
+        try {
+            return objectMapper.writeValueAsString(attribute);
+        } catch (JsonProcessingException e) {
+            throw new IllegalArgumentException("Error al convertir la lista a JSON", e);
+        }
+    }
+
+    @Override
+    public List<String> convertToEntityAttribute(String dbData) {
+        if (dbData == null || dbData.isBlank()) {
+            return new ArrayList<>(); // Devuelve una lista vacía
+        }
+        try {
+            return objectMapper.readValue(dbData, new TypeReference<List<String>>() {});
+        } catch (JsonProcessingException e) {
+            throw new IllegalArgumentException("Error al convertir el JSON a lista", e);
+        }
+    }
+}
