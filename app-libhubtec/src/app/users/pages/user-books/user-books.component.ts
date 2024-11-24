481b76024e6b02bafda14f2492aa311bfd1a00d2
Víctor J <maximovj@outlook.com>
2024-11-23 22:01:04 -0600

    * dev-angular-v18 : Se agrega libros para la página, ver mis libros reservados

--- app-libhubtec/src/app/users/pages/user-books/user-books.component.ts
+++ app-libhubtec/src/app/users/pages/user-books/user-books.component.ts
@@ -7,16 +7,28 @@ import { catchError, delay, map, Observable, of, startWith } from 'rxjs';
 import { AuthService } from '../../../auth/services/auth-service.service';
 import { BooksService } from './../../../books/services/books-service.service';
 import { LoadingComponent } from '../../../shared/components/loading/loading.component';
+import { ImageModule } from 'primeng/image';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { ThumbnailPipe } from '../../../books/pipes/thumbnail.pipe';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   standalone: true,
   imports: [
     CommonModule,
+    RouterModule,
+    ImageModule,
+    CardModule,
+    ButtonModule,
     LoadingComponent,
+    ThumbnailPipe,
   ],
   templateUrl: "./user-books.component.html",
 })
 export class UserBooksComponent implements OnInit {
+
+  private router = inject(Router);
 
   private authService = inject(AuthService);
 
@@ -27,14 +39,23 @@ export class UserBooksComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const user = this.authService.user();
     this.listReserveBook$ = this.booksService
-    .getListReservedBooksByAccount(this.authService.user())
+    .getListReservedBooksByAccount(user)
     .pipe(
       delay(1000),
-      map( data => ({ loading: false, data, err: null })),
+      map( data => {
+        console.log(data);
+        return { loading: false, data, err: null };
+      }),
       startWith({ loading: true, data: [], err: null }),
       catchError(err => of({ loading: false, data: [], err }))
     );
+  }
+
+  public bntGoTo(id :number) :void
+  {
+    this.router.navigateByUrl(`/books/p/${id}`);
   }
 
 }
